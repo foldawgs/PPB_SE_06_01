57c235787aa3d6d5bf8a38cdb12e1ea82a2c6a06
foldawgs <wahyu.isnantia08@gmail.com>
2024-10-24 12:03:12 +0700

    commit pertemuan 5

---
+++ Pertemuan_05/pertemuan5/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Hello, World!'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+              onPressed: () {
+                
+              },
+              child: const Text('Login'),
+              ),
+            ],
+            ),
+        ),
+      ),
+    );
+  }
+}
